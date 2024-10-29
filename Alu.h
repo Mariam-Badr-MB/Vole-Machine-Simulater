63dcf20663a1b296446e74a1a0c2194d4b61931f
Mariam-Badr-MB <mariambadr145@gmail.com>
2024-10-29 22:32:32 +0300

    Alu.h

--- Alu.h
+++ Alu.h
@@ -6,29 +6,34 @@ using namespace std;
 #define Alu_H
 
 class Alu {
-protected:
-    unordered_map<char, int> hexMap = {
-        {'0', 0}, {'1', 1}, {'2', 2}, {'3', 3}, {'4', 4}, {'5', 5}, {'6', 6},
-        {'7', 7}, {'8', 8}, {'9', 9}, {'A', 10}, {'B', 11}, {'C', 12},
-        {'D', 13}, {'E', 14}, {'F', 15}
-    };
-
 
 public:
-    void add(int idx1, int idx2, int idx3, Register &reg);
-    string hex_dec(const string &hex_number);
+    void add_Two_complement(int idx1, int idx2, int idx3, Register &reg);
+    void add_Floating_point(int idx1, int idx2, int idx3, Register &reg);
+    int hex_dec(const string &hex_number);
     string dec_hex(int number);
     bool isValid(const string &hex_number);
 };
 
 #endif //Alu_H
 
-
-void Alu::add(int idx1, int idx2, int idx3, Register &reg) {
-    reg.setCell(idx1, reg.getCell(idx2) + reg.getCell(idx3));
+void Alu::add_Two_complement(int idx1, int idx2, int idx3, Register &reg) {
+    reg.setcell(idx1, reg.getcell(idx2) + reg.getcell(idx3));
+}
+void Alu::add_Floating_point(int idx1, int idx2, int idx3, Register &reg) {
+    float s = *reinterpret_cast<float*>(reg.getcell(idx2)); 
+    float t = *reinterpret_cast<float*>(reg.getcell(idx3));
+    
+    float result = s + t ;
+    reg.setcell(idx1, *reinterpret_cast<int*>(&result)); 
 }
 
 bool Alu::isValid(const string &hex_number) {
+    unordered_map<char, int> hexMap = {
+        {'0', 0}, {'1', 1}, {'2', 2}, {'3', 3}, {'4', 4}, {'5', 5}, {'6', 6},
+        {'7', 7}, {'8', 8}, {'9', 9}, {'A', 10}, {'B', 11}, {'C', 12},
+        {'D', 13}, {'E', 14}, {'F', 15}
+    };
     for (char digit : hex_number) {
         if (hexMap.find(toupper(digit)) == hexMap.end()) {
             return false;
@@ -36,8 +41,12 @@ bool Alu::isValid(const string &hex_number) {
     }
     return true;
 }
-
-string Alu::hex_dec(const string &hex_number) {
+int Alu::hex_dec(const string &hex_number) {
+    unordered_map<char, int> hexMap = {
+        {'0', 0}, {'1', 1}, {'2', 2}, {'3', 3}, {'4', 4}, {'5', 5}, {'6', 6},
+        {'7', 7}, {'8', 8}, {'9', 9}, {'A', 10}, {'B', 11}, {'C', 12},
+        {'D', 13}, {'E', 14}, {'F', 15}
+    };
     int decimal_value = 0;
     int power = hex_number.length() - 1;
     for (char digit : hex_number) {
@@ -46,10 +55,4 @@ string Alu::hex_dec(const string &hex_number) {
     }
     return decimal_value;
 }
-
 string Alu::dec_hex(int number) {
-    if (number == 0) return "0";
-    stringstream ss;
-    ss << hex << uppercase << number;
-    return ss.str();
-}
