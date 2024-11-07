6cca17c7eed7a4afe71ee5e28902d6531cfeda04
z18409 <159724341+z18409@users.noreply.github.com>
2024-11-07 02:32:53 +0200

    Update Rotate.cpp

--- Rotate.cpp
+++ Rotate.cpp
@@ -1,25 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string Rotate_Right(int R_idx, int turns, Register& r)
+int Rotate_Right(int R_idx, int turns, Register& r)
 {
-    string bin = hex_binary(R_idx, r);
+   int dec = r.getcell(R_idx);
+    string bin = dec_bin(dec);
 
+    if(bin.size() < 8){
+        new_bin = "";
+        for(int i=bin.size(); i<8;i++){
+            new_bin += '0';
+        }
+        new_bin += bin;
+    }
+    else{
+        strcpy(new_bin, bin);
+    }
     for(int i=0; i<turns; i++)
     {
-        int temp1 = bin[7];
-        bin[7] = bin[6];
-        bin[6] = bin[5];
-        bin[5] = bin[4];
-        bin[4] = bin[3];
-        bin[3] = bin[2];
-        bin[2] = bin[1];
-        bin[1] = bin[0];
-        bin[0] = temp1;
+        int temp1 = new_bin[7];
+        new_bin[7] = new_bin[6];
+        new_bin[6] = new_bin[5];
+        new_bin[5] = new_bin[4];
+        new_bin[4] = new_bin[3];
+        new_bin[3] = new_bin[2];
+        new_bin[2] = new_bin[1];
+        new_bin[1] = new_bin[0];
+        new_bin[0] = temp1;
 
     }
-    string hexa = bin_hex(bin);
-    return hexa;
+    int dec2 = bin_dec(bin);
+    return dec2;
+
 }
 
 
