b8669e37ef56042b83df2d1750b4571b70f22ba9
z18409 <159724341+z18409@users.noreply.github.com>
2024-11-07 11:23:54 +0200

    Update Rotate.cpp

--- Rotate.cpp
+++ Rotate.cpp
@@ -1,35 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int Rotate_Right(int R_idx, int turns, Register& r)
 {
    int dec = r.getcell(R_idx);
-    string bin = dec_bin(dec);
-
-    if(bin.size() < 8){
-        new_bin = "";
-        for(int i=bin.size(); i<8;i++){
-            new_bin += '0';
-        }
-        new_bin += bin;
-    }
-    else{
-        strcpy(new_bin, bin);
-    }
-    for(int i=0; i<turns; i++)
-    {
-        int temp1 = new_bin[7];
-        new_bin[7] = new_bin[6];
-        new_bin[6] = new_bin[5];
-        new_bin[5] = new_bin[4];
-        new_bin[4] = new_bin[3];
-        new_bin[3] = new_bin[2];
-        new_bin[2] = new_bin[1];
-        new_bin[1] = new_bin[0];
-        new_bin[0] = temp1;
-
-    }
-    int dec2 = bin_dec(bin);
+   string bin = dec_bin(dec);
+   string new_bin = "";
+   while(bin.size() < 8){
+      bin = '0' + bin;
+   }
+   turns = turns%8;
+   new_bin += bin.substr(8-turns);
+   new_bin += bin.substr(0, 8-turns);
+    int dec2 = bin_dec(new_bin);
     return dec2;
 
 }
